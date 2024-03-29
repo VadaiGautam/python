4f3b8ce8988a60507aa9c3b418993995c3e2dc92
vadai gautam <vadaigautam0@.in>
2024-03-29 00:39:03 -0700

    add more codes

---
+++ basic-of-python-code/if__name__=main.py
@@ -0,0 +1,7 @@
+import vadai
+
+vadai.welcome()
+print(vadai.vadai)
+
+
+# this file is link with the vadai.py file
---
+++ basic-of-python-code/vadai.py
@@ -0,0 +1,14 @@
+def welcome():
+    print("hey how are you my friend! please welcome")
+
+vadai = "hi i am vadai"
+
+print(__name__)
+
+
+if __name__ == "__main__": # means this is the main file of program .... jab vadai.py run ho tabhi ye execute ho  oyherwise nhi ........
+ welcome()
+
+# jab ham vadai.py run karenge tab ...name ki value main aayegi ......
+ 
+#  or agar kisi or file me import hoga iss file ka function  or kisi or file se .vadai.py run hogi to name ki value  uss file ka name hoga to if condition false hone ki vajah se nhi chalegi ...
