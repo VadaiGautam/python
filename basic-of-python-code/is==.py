f8a1500e4382b326f9800c7946c675c0b176b565
vadai gautam <vadaigautam0@.in>
2024-04-03 10:32:30 -0700

    add is== file

---
+++ basic-of-python-code/is==.py
@@ -0,0 +1,56 @@
+a = 3
+b="3"
+
+print(a is b)  # same memory location pr hoga to true return hoga otherwise false
+print(a==b)   # VALUE EQUAL hogi to True return otherwise false 
+
+print(" ")
+
+a= 3
+b=4
+print(a is b) 
+print(a==b)
+
+print(" ")
+
+a=3
+b=3
+print(a is b)    # three is an int or constant value can not change that's why 3 store same location in the memory
+print(a==b)
+
+print(" ")
+
+a= 3
+b="3"
+print(a is b) 
+print(a==b)
+
+print(" ")
+
+a= "vadai"
+b="vadai"
+print(a is b) 
+print(a==b)
+
+print(" ")
+
+a= (1,2,3)
+b=(1,2,3)
+print(a is b) 
+print(a==b)
+
+print(" ")
+
+a= [1,2,3]
+b=[1,2,3]
+print(a is b) 
+print(a==b)
+
+print(" ")
+
+a= {1,2,3}
+b={1,2,3}
+print(a is b) 
+print(a==b)
+
+
